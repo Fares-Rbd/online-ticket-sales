06379fd9aa104db7d859743dc35e1dacb05424a8
Fares <faresraboudi308@gmail.com>
2024-11-24 13:08:30 +0100

    configure user management ms for eureka discovery

--- user_management/src/main/java/com/fares/user_management/UserManagementApplication.java
+++ user_management/src/main/java/com/fares/user_management/UserManagementApplication.java
@@ -2,6 +2,7 @@ package com.fares.user_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class UserManagementApplication {
